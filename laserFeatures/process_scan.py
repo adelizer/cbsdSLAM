7c407df3d0009e42804e90a8dc48c6d0d381eb56
adel <mohamed.adel03@gmail.com>
2019-02-17 00:00:18 +0100

    reshape and save scan data

--- laserFeatures/process_scan.py
+++ laserFeatures/process_scan.py
@@ -14,7 +14,11 @@ from sensor_msgs.msg import LaserScan
 class ScanSaver(object):
     def __init__(self):
         rospy.Subscriber('scan', LaserScan, self._scan_cb)
-        
+        self._data = None
+
+    def __del__(self):
+        np.save('scan_nparray', self._data)
+
     def _scan_cb(self, msg):
         rospy.logdebug("angle_min %f" %msg.angle_min)
         rospy.logdebug("angle_max %f" %msg.angle_max)
@@ -28,7 +32,16 @@ class ScanSaver(object):
         for i in range(len(msg.ranges)):
             x.append(msg.ranges[i] * np.cos(msg.angle_min + (i*msg.angle_increment)))
             y.append(msg.ranges[i] * np.sin(msg.angle_min + (i*msg.angle_increment))) 
-     
+        x = np.array(x)
+        y = np.array(y)
+        agg = np.array([x,y]).reshape(1,2,x.shape[0])
+
+        if self._data is None:
+            self._data = agg
+        else:
+            self._data = np.concatenate((self._data, agg), axis=0)
+
+        rospy.logdebug("data size {}".format(self._data.shape))
 
 
 def main():
